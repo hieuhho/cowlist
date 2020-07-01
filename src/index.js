110f892b5f12448e3de79921950917a258866ccd
Hieu Ho <hieuhho.dev@gmail.com>
2020-07-01 12:51:57 -0700

    Linter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const db = require('./db/index')
+const db = require('./db/index');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -13,71 +14,67 @@ app.use(bodyParser.json());
 app.get('/api/cows', (req, res) => {
   db.Cow.findAll()
     .then((cows) => {
-      res.status(200)
-      res.send(cows)
-      res.end()
+      res.status(200);
+      res.send(cows);
+      res.end();
     })
     .catch((err) => {
-      res.status(500)
-      res.end(err)
-    })
+      res.status(500);
+      res.end(err);
+    });
 });
 
-app.post('/api/cows', (req,res) => {
+app.post('/api/cows', (req, res) => {
   db.Cow.create(req.body.post)
     .then((cow) => {
-      res.status(200)
-      res.send(cow)
-      res.end()
+      res.status(200);
+      res.send(cow);
+      res.end();
     })
     .catch((err) => {
-      res.status(500)
-      res.end(err)
-    })
+      res.status(500);
+      res.end(err);
+    });
 });
 
 app.put('/api/cows/:id', (req, res) => {
-  cowID = path.basename(req.url);
-  db.Cow.update(req.body.post , {
+  const cowID = path.basename(req.url);
+  db.Cow.update(req.body.post, {
     where: {
-      id: cowID
-    }
+      id: cowID,
+    },
   })
-  .then(() => {
-    return db.Cow.findAll()
-  })
-  .then((cows) => {
-    res.status(200)
-    res.send(cows)
-    res.end()
-  })
-  .catch((err) => {
-    res.status(500)
-    res.end(err)
-  })
+    .then(() => db.Cow.findAll())
+    .then((cows) => {
+      res.status(200);
+      res.send(cows);
+      res.end();
+    })
+    .catch((err) => {
+      res.status(500);
+      res.end(err);
+    });
 });
 
 app.delete('/api/cows/:id', (req, res) => {
-  cowID = path.basename(req.url);
+  const cowID = path.basename(req.url);
   db.Cow.destroy({
     where: {
-      id: cowID
-    }
+      id: cowID,
+    },
   })
-  .then(() => {
-    return db.Cow.findAll()
-  })
-  .then((cows) => {
-    res.status(200)
-    res.send(cows)
-    res.end()
-  })
-  .catch((err) => {
-    res.status(500)
-    res.end(err)
-  })
+    .then(() => db.Cow.findAll())
+    .then((cows) => {
+      res.status(200);
+      res.send(cows);
+      res.end();
+    })
+    .catch((err) => {
+      res.status(500);
+      res.end(err);
+    });
 });
 
-let port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('Server ready'));
+app.listen(port, () => console.log(`Server ready on ${port}`));
