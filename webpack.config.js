7dd95c1a15cf50a7109df72360302a46bdb7a6de
Hieu Ho <hieuhho.dev@gmail.com>
2020-07-01 12:01:43 -0700

    Added linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 const path = require('path');
 
-
 module.exports = {
   entry: './src/client/index.js',
   output: {
     path: path.resolve(__dirname, './src/public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    },
+    ],
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
 };
