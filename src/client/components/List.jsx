08b9c5831b86463ed6665682d68e1fa5a27dc993
Hieu Ho <hieuhho.dev@gmail.com>
2020-07-01 12:50:51 -0700

    Added button types

--- src/client/components/List.jsx
+++ src/client/components/List.jsx
@@ -5,15 +5,15 @@ const List = ({
 }) => (
   <div>
     <h2>
-      Da Best Cows
+      Best Cows
     </h2>
     <ul>
       {cowList.map((cow) => (
         <li key={cow.id} onClick={() => description(`${cow.name}: ${cow.description}`)}>
           {cow.name}
           <span className="buttons">
-            <button type="button" className="editButton" onClick={() => updateCow(`${cow.id}`)}>pimp ur cow</button>
-            <button type="button" className="deleteButton" onClick={() => deleteCow(`${cow.id}`)}>destroy cow</button>
+            <button type="button" className="editButton" onClick={() => updateCow(`${cow.id}`)}>Edit Cow</button>
+            <button type="button" className="deleteButton" onClick={() => deleteCow(`${cow.id}`)}>Destroy Cow</button>
           </span>
         </li>
       ))}
